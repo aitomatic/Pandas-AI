14e996ee7a5a808a4d5e1f6314c6cd8f64199318
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@GMail.com>
2023-06-19 10:54:45 -0700

    update Baidu LLM submodule

--- pandasai/llm/baidu.py
+++ pandasai/llm/baidu.py
@@ -1,1 +1,19 @@
-"""Baidu ERNIE Bot LLM."""
+"""Baidu Ernie Bot LLM.
+
+This module is to run the Baidu Ernie Bot LLM API.
+"""
+
+
+from .base import LLM
+
+
+class BaiduErnieBot(LLM):
+    """Baidu Ernie Bot LLM."""
+
+    model: str = "models/text-bison-001"
+
+    def __init__(self, api_key: str, **kwargs):
+        """Initialize Baidu Ernie Bot LLM connection."""
+
+    def call(self, instruction: str, value: str, suffix: str = "") -> str:
+        """Call Baidu Ernie Bot LLM."""
